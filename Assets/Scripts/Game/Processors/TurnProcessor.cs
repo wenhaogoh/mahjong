d4f468a5af6873d2f796f70095c253d591714276
wenhao <wenhaogoh1998@gmail.com>
2021-05-24 14:30:51 +0800

    Fix trigger tile not being removed from discarded tiles container when executing a request

--- Assets/Scripts/Game/Processors/TurnProcessor.cs
+++ Assets/Scripts/Game/Processors/TurnProcessor.cs
@@ -135,6 +135,7 @@ public class TurnProcessor
             }
             else
             {
+                discardedTilesContainer.RemoveLastTile();
                 ExecuteRequest(request);
             }
         }
